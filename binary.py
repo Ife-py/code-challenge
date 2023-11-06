c7f372f087799d61b6794338b8687d39812f9d21
Ife <139559743+Ife-py@users.noreply.github.com>
2023-11-06 23:59:53 +0100

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,9 +1,11 @@
-b= int(input("enter number to be converted to decimal\n"))
+#simple cocde to convert decimal to binary
+b= int(input("enter number to be converted to binary\n"))
 final = ""
 
 while b > 0:
     reminder = b % 2
     u = str(reminder)
     final=u+final
+    #floor division of our final re
     b =b //2
 print(f"The conversion of {b} to a binary is {final}")
