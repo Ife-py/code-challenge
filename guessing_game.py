1a13b4eca5a54078fd77f8838361466dad4492c1
Ife <139559743+Ife-py@users.noreply.github.com>
2023-11-06 23:54:09 +0100

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random
 #Guesssing game in python
-
+#this line takes in input 
 count=""
 prompt=""
 print("Welcome to a guessing game personally designed by Ife.py")
@@ -8,7 +8,9 @@ print("To stop guessing press exit to stop")
 game_complexity=input("what level do you wanna play EASY,MEDIUM or HARD\n").lower
 outer_counter=1
 play_again=''
+#i created an outer loop to loop through the inner code for a number of time
 while play_again!="no"or"No"or"NO" and outer_counter==5:
+    #game complexity for easy
     if game_complexity=="easy":
         print("you have to guess numbers from range 0 to 20")
         f=random.randrange(0,20)
@@ -27,6 +29,7 @@ while play_again!="no"or"No"or"NO" and outer_counter==5:
                 print("you guessed correctly")
                 break
         print(f"correct answer is {f}")
+        #game complexity for medium
     elif game_complexity=="medium":
         print("you have to guess numbers from range 0 to 50")
         g=random.randrange(0,50)
@@ -44,6 +47,7 @@ while play_again!="no"or"No"or"NO" and outer_counter==5:
                 print("you guessed correctly")
                 break            
         print(f"correct answer is {g}")
+        #game complexity for HARD
     elif game_complexity=="hard":
         print("you have to guess numbers from range 0 to 70")
         h=random.randrange(0,70)
@@ -59,8 +63,10 @@ while play_again!="no"or"No"or"NO" and outer_counter==5:
             elif prompt== h:
                 print("you guessed correctly")
                 break
-        print(f"correct answer is {h}") 
+        print(f"correct answer is {h}")
+        #OUTPUT TO BE GENERATED
     play_again=input("do you want to play again Yes or No\n").lower
+    #while loop if you want to play again
     if play_again=="yes":
         game_complexity=input("what level do you wanna play EASY,MEDIUM or HARD\n")
     elif play_again=="no":
